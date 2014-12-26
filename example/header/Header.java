8b9e57c2bfa32bd0dc30758db7210213304dd619
Yu-Ting Chen <timmy00274672@gmail.com>
2014-12-26 16:14:56 +0800

    Add Header example.

---
+++ example/header/Header.java
@@ -0,0 +1,7 @@
+public class Header{
+	public static native void a();
+	public native void b();
+	public static native int c();
+	public native void d(int para);
+	public native void e(String para);
+}
